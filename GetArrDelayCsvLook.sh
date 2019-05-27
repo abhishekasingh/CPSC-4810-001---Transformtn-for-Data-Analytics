f12ebc2a9b90ce8c0efd4a4ab44b4a4a5c6e46df
ubuntua <abhishek.ar.singh@gmail.com>
2019-05-26 18:08:44 -0700

    File updated with name

--- GetArrDelayCsvLook.sh
+++ GetArrDelayCsvLook.sh
@@ -1,6 +1,6 @@
-#Author: Abhishke Singh
+#Author: Abhishek Singh
 #!/usr/bin/env bash
 
-echo 'Abhishek'
+echo 'Abhishek Singh here..!!'
 
 csvcut -c 15,17 flightsdelay.csv | csvgrep -c2 -m SFO | head -n 4 > first3sfo.csv
